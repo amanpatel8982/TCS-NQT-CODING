9dd41dd56ab0aaae0e76a5a4527a5160da35325d
Aman Patel <ap7463015@gmail.com>
2026-01-01 11:08:51 +0530

    Add Que2 and Que3 programs for string case conversion

---
+++ StringAssesment/Que2.java
@@ -0,0 +1,19 @@
+package StringAssesment;
+
+// Que 2. Write a Java program to convert a string to
+// uppercase.
+// Input: "java"
+// Output: "JAVA"
+
+
+public class Que2 
+{
+  public static void main(String[] args) 
+  {
+    String str = "aman patel";
+    String upper = str.toUpperCase();
+    System.out.println("Upper of str : " + upper);
+    
+  }
+    
+}
---
+++ StringAssesment/Que3.java
@@ -0,0 +1,20 @@
+package StringAssesment;
+
+
+// Que 3. Write a Java program to convert a given string
+// to lowercase.
+// Input: "JAVA"
+// Output: "java"
+
+public class Que3 
+{
+    public static void main(String[] args) 
+    {
+        String str = "Aman Patel";
+        String lowerCase = str.toLowerCase();
+        System.out.println(lowerCase);
+
+        
+    }
+    
+}
