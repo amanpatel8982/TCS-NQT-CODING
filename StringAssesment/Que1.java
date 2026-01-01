a21f28086f0b50d5dd1b86adf20d6d6cb37516ce
Aman Patel <ap7463015@gmail.com>
2026-01-01 10:57:00 +0530

    legth of String

---
+++ StringAssesment/Que1.java
@@ -0,0 +1,20 @@
+package StringAssesment;
+
+
+// Que 1. Write a Java program to find the length of a
+// given string.
+// Input: "Java Programming"
+// Output: 16
+
+
+public class Que1 
+{
+  public static void main(String[] args)
+  {
+
+    String str = "Java Programming";
+    int length = str.length();
+    System.out.println("The length of the given string is: " + length);
+  }
+    
+}
