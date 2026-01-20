9aaf91e1c2d8d6bf56a2ecf4aa75cc747195d0a2
Aman Patel <ap7463015@gmail.com>
2026-01-21 01:34:40 +0530

    Add TwoSum program to find two numbers that add up to a target

---
+++ Array/TwoSum.java
@@ -0,0 +1,47 @@
+package Array;
+
+public class TwoSum 
+{
+
+    public static int sum(int target,int num[])
+    {
+         
+        int array = 0;
+        for (int i = 0; i < num.length; i++) 
+        {
+            for (int j =  i + 1 ; j < num.length; j++) 
+            {
+                if(num[i] + num[j] == target)
+                {
+                  array = num[i] + num[j];
+                }
+            }
+        }
+
+        return array;
+    }
+    public static void main(String[] args) 
+    {
+        int num[] =  {2, 7, 11, 15};
+        int target = 9;
+
+        System.out.println(sum(target, num));
+
+        
+    }
+    
+}
+
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         for (int i = 0; i < nums.length - 1; i++) {
+//             for (int j = i + 1; j < nums.length; j++) {
+//                 if (nums[i] + nums[j] == target) {
+//                     return new int[]{i, j};
+//                 }
+//             }
+//         }
+//         return new int[]{}; // No solution found
+//     }
+// }
